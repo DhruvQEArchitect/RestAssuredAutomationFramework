f68769d45c22d77a6f8830912e58f8d8adfd11e3
Dhruv <dhruv.kmr.2101@gmail.com>
2025-07-10 20:22:41 -0400

    Update ProductTests.java

--- src/test/java/tests/ProductTests.java
+++ src/test/java/tests/ProductTests.java
@@ -13,13 +13,13 @@ import pojo.Mapper;
 @Listeners(ReportingListeners.class)
 public class ProductTests {
 
-    @Test
+   /* @Test
     public void AddProduct() {
         Response response = ResponseUtils.postResponse(DynamicPayloads.getProductPayload()).prettyPeek();
         Assert.assertEquals(response.statusCode(), 200);
         Logging.logInfo("Response code verified successfully as {},", response.statusCode());
         Assert.assertEquals(Mapper.getProductResponsePayload(response), DynamicPayloads.getProductPayload());
         Logging.logInfo("Response verified successfully as {},", response.prettyPrint());
-    }
+    }*/
 
 }
